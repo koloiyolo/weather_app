71838683e4f88602b21d582a4cf04ce85453d392
Jakub Kolodziej <jakvb.kolodziej@gmail.com>
2023-04-15 16:59:52 +0200

    added "Time of the day"

--- lib/elements/weathertile.dart
+++ lib/elements/weathertile.dart
@@ -16,33 +16,95 @@ class WeatherTile extends StatelessWidget {
           const SizedBox(height: 16),
           Row(
             children: [
-             const Expanded(flex: 2,child: SizedBox()),
-             BuildText(
+              const Expanded(flex: 2, child: SizedBox()),
+              BuildText(
                   text: farenheit
                       ? '${(data['current']['temp'] * 1.8 + 32).round()}°F'
                       : '${(data['current']['temp']).round()}°C',
                   size: 2.5),
               const Expanded(flex: 1, child: SizedBox()),
-              Image.asset('lib/assets/${data['current']['weather'][0]['icon']}@2x.png'),
-              const Expanded(flex: 2,child: SizedBox())
-                  ],
-          ),
-          BuildText(text: data['current']['weather'][0]['main'],size: 2),
-          BuildText(
-                  text: farenheit
-                      ? 'Percived temperature: ${(data['current']['feels_like'] * 1.8 + 32).round()}°F'
-                      : 'Percived temperature: ${(data['current']['feels_like']).round()}°C',
-                  size: 1.3),
-                  const SizedBox(height: 64),
-          ExpansionTile(
-            title: BuildText(text: 'More details', size: 1.5),
-            children: [
-              BuildText(text: 'Pressure: ${data['current']['pressure']} hPa', size: 1.3),
-              BuildText(text: 'Clouds density: ${data['current']['clouds']}%',size: 1.3),
-              BuildText(text: 'Wind Speed: ${data['current']['wind_speed']} m/s', size: 1.3),
-
+              Image.asset(
+                  'lib/assets/${data['current']['weather'][0]['icon']}@2x.png'),
+              const Expanded(flex: 2, child: SizedBox())
             ],
-          )
+          ),
+          BuildText(text: data['current']['weather'][0]['main'], size: 2),
+          BuildText(
+              text: farenheit
+                  ? 'Percived temperature: ${(data['current']['feels_like'] * 1.8 + 32).round()}°F'
+                  : 'Percived temperature: ${(data['current']['feels_like']).round()}°C',
+              size: 1.3),
+          ExpansionTile(
+
+            title: BuildText(text: 'Time of the day', size: 1.5),
+
+            children: [
+              Row(children: [
+                BuildText(
+                    text: farenheit
+                        ? ' ${(data['daily'][0]['temp']['morn'] * 1.8 + 32).round()}°F'
+                        : ' ${(data['daily'][0]['temp']['morn']).round()}°C',
+                    size: 1.3),
+                const Expanded(flex: 1, child: SizedBox()),
+                BuildText(
+                    text: farenheit
+                        ? ' ${(data['daily'][0]['temp']['day'] * 1.8 + 32).round()}°F'
+                        : ' ${(data['daily'][0]['temp']['day']).round()}°C',
+                    size: 1.3),
+                const Expanded(flex: 1, child: SizedBox()),
+                BuildText(
+                    text: farenheit
+                        ? ' ${(data['daily'][0]['temp']['eve'] * 1.8 + 32).round()}°F'
+                        : ' ${(data['daily'][0]['temp']['eve']).round()}°C',
+                    size: 1.3),
+                const Expanded(flex: 1, child: SizedBox()),
+                BuildText(
+                    text: farenheit
+                        ? ' ${(data['daily'][0]['temp']['night'] * 1.8 + 32).round()}°F'
+                        : ' ${(data['daily'][0]['temp']['night']).round()}°C',
+                    size: 1.3),
+              ]),
+              Row(
+                children: [
+                  BuildText(text: '6:00 - 12:00\nAM', size: 1.1),
+                  const Expanded(flex: 1, child: SizedBox()),
+                  BuildText(text: '12:00-6:00\nPM', size: 1.1),
+                  const Expanded(flex: 1, child: SizedBox()),
+                  BuildText(text: '6:00 - 12:00\nPM', size: 1.1),
+                  const Expanded(flex: 1, child: SizedBox()),
+                  BuildText(text: '12:00-6:00\nAM', size: 1.1),
+                ],
+              ),
+              Row(
+                children: [
+                  BuildText(text: 'Morning', size: 1.3),
+                  const Expanded(flex: 1, child: SizedBox()),
+                  BuildText(text: 'Afternoon', size: 1.3),
+                  const Expanded(flex: 1, child: SizedBox()),
+                  BuildText(text: 'Evening', size: 1.3),
+                  const Expanded(flex: 1, child: SizedBox()),
+                  BuildText(text: 'Night', size: 1.3),
+                ],
+              )
+            ],
+          ),
+          ExpansionTile(
+
+            title: BuildText(text: 'More details', size: 1.5),
+
+            children: [
+              BuildText(
+                  text: 'Pressure: ${data['current']['pressure']} hPa',
+                  size: 1.3),
+              BuildText(
+                  text: 'Clouds density: ${data['current']['clouds']}%',
+                  size: 1.3),
+              BuildText(
+                  text: 'Wind Speed: ${data['current']['wind_speed']} m/s',
+                  size: 1.3)
+            ],
+          ),
+          
         ],
       ),
     );
